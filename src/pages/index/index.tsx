6094a3884b069ba96776d0c6582ee4e5464f2077
zinai <zinai@inceptionpad.com>
2023-02-16 00:49:17 +0800

    init

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from '@tarojs/components'
+import './index.scss'
+
+export default function Index() {
+
+
+  return (
+    <View className='index'>
+      <Text>Hello world!</Text>
+    </View>
+  )
+
+}
