4db2dab67a7a0e6cc063ff5afe5027b498bc9200
zinai <zinai@inceptionpad.com>
2023-02-16 01:40:09 +0800

    feat: ui demo

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,21 +1,124 @@
 import {
-  Button,
-} from "@nutui/nutui-react-taro";import './index.scss'
+  Cell,
+  Icon,
+  Infiniteloading,
+  NavBar,
+  Notify,
+  Tabbar,
+  TabbarItem,
+} from "@nutui/nutui-react-taro";
+import { useEffect, useState } from "react";
+import './index.scss'
+const InfiniteUlStyle = {
+  height: '300px',
+  width: '100%',
+  padding: '0',
+  overflowY: 'auto',
+  overflowX: 'hidden'
+}
 
+const InfiniteLiStyle = {
+  marginTop: '10px',
+  fontSize: '14px',
+  color: 'rgba(100, 100, 100, 1)',
+  textAlign: 'center'
+}
 export default function Index() {
+  const [defultList, setDefultList] = useState<string[]>([])
+  const [hasMore, setHasMore] = useState(true)
+  const [showNotify, SetShowNotify] = useState(false)
+  const [states, SetStates] = useState({
+    msg: '',
+    type: 'danger',
+  })
 
+  useEffect(() => {
+    init()
+  }, [])
+  const changeNotify = (msg: string, type?: string) => {
+    const change = Object.assign(states, { msg, type })
+    SetStates(change)
+  }
+  const loadMore = (done: () => void) => {
+    setTimeout(() => {
+      const curLen = defultList.length
+      for (let i = curLen; i < curLen + 10; i++) {
+        defultList.push(`${i}`)
+      }
+      if (defultList.length >= 30) {
+        setHasMore(false)
+      } else {
+        setDefultList([...defultList])
+      }
+      done()
+    }, 500)
+  }
+
+  const init = () => {
+    for (let i = 0; i < 10; i++) {
+      defultList.push(`${i}`)
+    }
+    setDefultList([...defultList])
+  }
 
   return (
-    <div className="nutui-react-demo">
-      <div className="index">
-       欢迎使用 NutUI React 开发 Taro 多端项目。
+    <>
+      <NavBar
+        title="订单详情"
+        leftShow
+        leftText="返回"
+        onClickTitle={(e) => alert("标题")}
+        onClickBack={(e) => alert("返回")}
+        onClickRight={(e) => alert('icon')}
+      >
+        <Icon name="share" slot="right" />
+      </NavBar>
+      <div>
+        <Notify
+          visible={showNotify}
+          msg={states.msg}
+          type={states.type}
+          onClosed={() => {
+            SetShowNotify(false)
+          }}
+          onClick={() => {
+            console.log('click')
+          }}
+        />
+        <Cell
+          title="基础用法"
+          onClick={(event: React.MouseEvent) => {
+            changeNotify('成功通知','success')
+            SetShowNotify(true)
+          }}
+        />
+        <Cell>
+          <ul id="scroll" style={InfiniteUlStyle}>
+            <Infiniteloading
+              containerId="scroll"
+              useWindow={false}
+              hasMore={hasMore}
+              loadMore={loadMore}
+            >
+              {defultList.map((item, index) => {
+                return (
+                  <li key={index} style={InfiniteLiStyle}>
+                    {item}
+                  </li>
+                )
+              })}
+            </Infiniteloading>
+          </ul>
+        </Cell>
       </div>
-      <div className="index">
-        <Button type="primary" className="btn">
-          NutUI React Button
-        </Button>
-      </div>
-    </div>
+      <Tabbar bottom>
+        <TabbarItem tabTitle="首页" href="" icon="home" />
+        <TabbarItem tabTitle="分类" icon="category" />
+        <TabbarItem tabTitle="发现" icon="find" />
+        <TabbarItem tabTitle="购物车" href="https://m.jd.com" icon="cart" />
+        <TabbarItem tabTitle="我的" href="/" icon="my" />
+      </Tabbar>
+    </>
   );
 
 }
